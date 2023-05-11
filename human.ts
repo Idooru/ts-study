49906198b2e9b9da4d3d330867dadcd46d0f8f07
idooru <shere1765@gmail.com>
2023-05-11 19:27:04 +0900

    #상속

---
+++ human.ts
@@ -0,0 +1,31 @@
+/**
+ * #상속
+ */
+
+class Human {
+  constructor(
+    protected readonly name: string,
+    protected readonly age: number
+  ) {}
+
+  introduce(): string {
+    return `Hi I'm ${this.name} and I'm ${this.age}years old`;
+  }
+}
+
+class Developer extends Human {
+  constructor(protected readonly name: string, protected readonly age: number) {
+    console.log(name);
+    console.log(age);
+    super(name, age);
+  }
+
+  introduce(): string {
+    const greeting = super.introduce();
+    return greeting.concat(" and do code");
+  }
+}
+
+const dev: Human = new Developer("Idooru", 26);
+const result = dev.introduce();
+console.log(result);
